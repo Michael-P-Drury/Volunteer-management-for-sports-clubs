e7e1285199f7c816eff85a014be89500175a3c5f
Michael Drury <mpdrury15@gmail.com>
2023-10-30 14:01:13 +0000

    databases file update

--- databases.py
+++ databases.py
@@ -1,10 +1,7 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
-# sets up the website, the databases and the login for the websites
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'password'
@@ -12,12 +9,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
-
-# Usertable which also stores the users wishlist and cart as a string of item ids split up by spaces
-
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
     password_hash = db.Column(db.String(64))
+    email = db.Column(db.String(30))
+    mobile = db.Column(db.String(15))
+    preferred_name = db.Column(db.String(15))
+    qualifications = db.Column(db.String(64))
+    events = db.Column(db.String(100))
 
+class jobs(UserMixin, db.Model):
+    __tablename__ = 'jobs'
+    jobid = db.Column(db.Integer, primary_key=True)
+    volunteers_assigned = db.Column(db.String(300))
+    volunteers_needed = db.Column(db.Integer)
+    times = db.Column(db.String(20))
+    date = db.Column(db.String(20))
+    job_description = db.Column(db.String(300))
+    job_requirements = db.Column(db.String(200))
