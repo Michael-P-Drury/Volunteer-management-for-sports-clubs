4013bbd6300c3e182edeb5d89a0e5e06d7c28fd7
Michael Drury <mpdrury15@gmail.com>
2023-10-31 15:17:57 +0000

    debug as layout page was missing the container for the page and different pages were missing their URLs

--- website/routes.py
+++ website/routes.py
@@ -3,16 +3,17 @@ from flask_login import login_required, login_user, logout_user, current_user
 from .databases import User
 from . import app, lm
 from .databases import Jobs
+from . import db
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/pick_club', methods=['GET', 'POST'])
 def pick_club():
     return render_template('pick_club.html')
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
