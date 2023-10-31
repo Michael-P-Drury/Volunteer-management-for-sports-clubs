6467a09464cc51e6d2fb929c276bf240456faf13
Michael Drury <mpdrury15@gmail.com>
2023-10-31 14:28:37 +0000

    added innit file

---
+++ website/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# sets up the website, the databases and the login for the websites
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.login_view = 'login'
+app.app_context().push()
+
+from . import routes
