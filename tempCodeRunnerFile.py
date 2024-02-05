2e3744363c22289c9e1831d5dc8ceaa2c369334a
Brandon Williams <Bwilliams56@cardiff.ac.uk>
2024-02-05 14:08:24 +0000

    Homepage Improvement

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+from website import app, db
+from website.databases import User
+
+# run file which calls the application file website running the website
+# creates a login for use when testing with username michael and password michael1
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        if User.query.filter_by(username='michael').first() is None:
+            User.register('michael', 'michael1', False)
+    app.run(debug = True)
