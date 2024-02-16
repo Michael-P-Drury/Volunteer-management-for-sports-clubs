6c560f5cc6befbdf18ab40a759ec2ad0fe59d531
Brandon Williams <Bwilliams56@cardiff.ac.uk>
2024-02-16 12:19:59 +0000

    Added Edit button to hide mobile change

--- website/static/edit-buttons.js
+++ website/static/edit-buttons.js
@@ -1,7 +1,16 @@
 function editEmails() {
-    var T = document.getElementById("profile-box-text"),
+    var T = document.getElementById("profile-box-email"),
         displayValue = "";
     if (T.style.display == "")
+        displayValue = "none";
+
+    T.style.display = displayValue;
+}
+
+function editMobile() {
+    var T = document.getElementById("profile-box-mobile"),
+        displayValue = "";
+    if (T.style.display =="")
         displayValue = "none";
 
     T.style.display = displayValue;
