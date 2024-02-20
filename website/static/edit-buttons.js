64a80f27075687332deb0f4fcea06505eb5451db
Michael Drury <mpdrury15@gmail.com>
2024-02-20 16:09:12 +0000

    fixed bug with mobile and email remove button (they didnt have their own crf token)

--- website/static/edit-buttons.js
+++ website/static/edit-buttons.js
@@ -15,21 +15,3 @@ function editMobile() {
 
     T.style.display = displayValue;
 }
-
-function editRemoveEmails() {
-    var T = document.getElementById("profile-box-remove-email"),
-        displayValue = "";
-    if (T.style.display == "")
-        displayValue = "none";
-
-    T.style.display = displayValue;
-}
-
-function editRemoveMobile() {
-    var T = document.getElementById("profile-box-remove-mobile"),
-        displayValue = "";
-    if (T.style.display =="")
-        displayValue = "none";
-
-    T.style.display = displayValue;
-}
