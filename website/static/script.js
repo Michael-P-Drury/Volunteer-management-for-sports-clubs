04e1d3fca18d3f401cbfd74b090741f7501c58a9
jackThurman <jackthurman8@gmail.com>
2024-03-04 14:28:34 +0000

    Fixed js error

--- website/static/script.js
+++ website/static/script.js
@@ -1,24 +1,21 @@
-document.getElementById("defaultOpen").click();
-
 var slideIndex = 0;
-showSlides();
 
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    
+
     for (i = 0; i < slides.length; i++) {
-        /*slides[i].style.display = 'none';*/
         slides[i].style.opacity = 0;
     }
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0}
+    
+    slides[slideIndex].style.opacity = 1;
     slideIndex++;
 
-    if (slideIndex > slides.length) {
-        slideIndex = 1}
-
-    /*slides[slideIndex-1].style.display = 'block';*/
-    slides[slideIndex-1].style.opacity = 1;
     setTimeout(showSlides, 3000);
+    return 'Slideshow is running'
 }
 
 function expand(button) {
